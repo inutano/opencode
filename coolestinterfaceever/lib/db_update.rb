4288f88d2d5e27651d4dbc6b46e7a86c2fa3c38f
tazro inutano ohta <inutano@gmail.com>
2012-12-21 15:30:12 +0900

    changed db scheme

--- coolestinterfaceever/lib/db_update.rb
+++ coolestinterfaceever/lib/db_update.rb
@@ -10,15 +10,19 @@ class ProfileInit < ActiveRecord::Migration
     create_table(:profiles) do |t|
       t.string :gene_name, :null => false, :limit => 20
       t.string :gene_id, :null => false
-
-      t.string :expression, :null => false, :limit => 5
-      t.float :pvalue, :null => false
-      t.string :ucsc_url, :null => false
+      t.float :c1_tss1500
+      t.float :c1_tss200
+      t.float :c1_gb
+      t.float :c2_tss1500
+      t.float :c2_tss200
+      t.float :c2_gb
+      t.float :c3_tss1500
+      t.float :c3_tss200
+      t.float :c3_gb
       t.timestamps
     end
     add_index :profiles, :gene_name, :name => :genename_idx
     add_index :profiles, :gene_id, :name => :geneid_idx
-    add_index :profiles, :exp_acc, :name => :expacc_idx
   end
   def self.down
     drop_table(:profiles)
@@ -42,17 +46,28 @@ if __FILE__ == $0
     ProfileInit.migrate(:down)
     
   when "--insert"
-    csv_path = ARGV[1]
-    csv = CSV.read(csv_path)
-    rm_header = csv.shift
+    file_path = ARGV[1]
+    if ARGV[1] =~ /\.csv$/
+      lines = CSV.read(file_path)
+      rm_header = lines.shift
+    
+    elsif ARGV[1] =~ /\.tsv$/
+      lines = open(file_path).readlines.map{|l| l.split("\t")}
+      rm_header = lines.shift
+    end
     
     inserts = csv.map do |row|
       { gene_name: row.first,
         gene_id: row[1],
-        exp_acc: row[2],
-        expression: row[3],
-        pvalue: row[4],
-        ucsc_url: row[5] }
+        c1_tss1500: row[2],
+        c1_tss200: row[3],
+        c1_gb: row[4],
+        c2_tss1500: row[5],
+        c2_tss200: row[6],
+        c2_gb: row[7],
+        c3_tss1500: row[8],
+        c3_tss200: row[9],
+        c3_gb: row[10] }
     end
     
     Profile.transaction do
