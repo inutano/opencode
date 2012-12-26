eff07a7c25f6e20b79c9c40c56b009a7ee809303
tazro inutano ohta <inutano@gmail.com>
2012-12-26 13:23:02 +0900

    modified app as it works at least for now

--- coolestinterfaceever/lib/db_update.rb
+++ coolestinterfaceever/lib/db_update.rb
@@ -8,7 +8,7 @@ require "csv"
 class ProfileInit < ActiveRecord::Migration
   def self.up
     create_table(:profiles) do |t|
-      t.string :gene_name, :null => false, :limit => 20
+      #t.string :gene_name
       t.string :gene_id, :null => false
       t.float :c1_tss1500
       t.float :c1_tss200
@@ -21,7 +21,7 @@ class ProfileInit < ActiveRecord::Migration
       t.float :c3_gb
       t.timestamps
     end
-    add_index :profiles, :gene_name, :name => :genename_idx
+    #add_index :profiles, :gene_name, :name => :genename_idx
     add_index :profiles, :gene_id, :name => :geneid_idx
   end
   def self.down
@@ -49,30 +49,41 @@ if __FILE__ == $0
     file_path = ARGV[1]
     if ARGV[1] =~ /\.csv$/
       lines = CSV.read(file_path)
-      rm_header = lines.shift
+      #rm_header = lines.shift
     
     elsif ARGV[1] =~ /\.tsv$/
       lines = open(file_path).readlines.map{|l| l.split("\t")}
       rm_header = lines.shift
     end
     
-    inserts = csv.map do |row|
-      { gene_name: row.first,
-        gene_id: row[1],
-        c1_tss1500: row[2],
-        c1_tss200: row[3],
-        c1_gb: row[4],
-        c2_tss1500: row[5],
-        c2_tss200: row[6],
-        c2_gb: row[7],
-        c3_tss1500: row[8],
-        c3_tss200: row[9],
-        c3_gb: row[10] }
+    inserts = lines.map do |row|
+      # { gene_name: row.first,
+      { gene_id: row[0],
+        c1_tss1500: row[1],
+        c1_tss200: row[2],
+        c1_gb: row[3],
+        c2_tss1500: row[4],
+        c2_tss200: row[5],
+        c2_gb: row[6],
+        c3_tss1500: row[7],
+        c3_tss200: row[8],
+        c3_gb: row[9] }
     end
     
     Profile.transaction do
       inserts.each do |insert|
         Profile.create(insert)
+      end
+    end
+  
+  when "-f"
+    c1_path = ARGV[1]
+    c2_path = ARGV[2]
+    c3_path = ARGV[3]
+    
+    [c1_path, c2_path, c3_path].each do |csv|
+      rows = CSV.read(csv)
+      rows.each do ||
       end
     end
   end
